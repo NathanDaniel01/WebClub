1b3c77a8f9cacce904d2a0ad1fc05ba09b1daa8d
Jacob Hansen <thatoneheros451@gmail.com>
2021-02-07 02:02:19 -0700

    Reformat and optimization of code       - Added download protection to       - Added higlight prevention       - Other stuff too that I probably forgot

---
+++ javascript/scripts.js
@@ -0,0 +1,7 @@
+// index.html JavaScript
+
+//Auto Scroll - should be changed to a more generic function in the near future
+function scrollTo1() {
+    document.getElementById('scrollTo1').scrollIntoView({behavior: 'smooth'});
+}
+
